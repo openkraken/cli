e42bb5d447e0b00143f2fa2d6add1817663b6ade
chenghuai.dtc <chenghuai.dtc@alibaba-inc.com>
2021-03-24 15:10:34 +0800

    feat: throw error for windows

--- install.js
+++ install.js
@@ -3,6 +3,11 @@ const os = require('os');
 const path = require('path');
 const packageJSON = require('./package.json');
 
+
+if (os.platform() == 'win32') {
+  throw new Error('Kraken-Cli for Windows platform is not supported.');
+} 
+
 const tarName = `kraken-${os.platform()}-${packageJSON.version}.tar.gz`;
 const downloadUrl = `https://kraken.oss-cn-hangzhou.aliyuncs.com/kraken-cli-vendors/${tarName}`;
 
