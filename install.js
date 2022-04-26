df605a1374e588da3b7852b11af630d1913fe188
JinWeiyang <jwy8645@163.com>
2022-04-26 10:45:43 +0800

    fix: the property `cwd` in `processOptions`

--- install.js
+++ install.js
@@ -6,11 +6,13 @@ const packageJSON = require('./package.json');
 const tarName = `kraken-${os.platform()}-${packageJSON.version}.tar.gz`;
 const downloadUrl = `https://kraken.oss-cn-hangzhou.aliyuncs.com/kraken-cli-vendors/${tarName}`;
 
+const wrapPath = (path) => path.replace(/ /g, "\\ ");
+
 const processOptions = {
-  cwd: __dirname,
+  cwd: wrapPath(__dirname),
   stdio: 'inherit'
 };
 execSync(`curl -O ${downloadUrl}`, processOptions);
 execSync('mkdir -p build', processOptions);
-execSync(`tar xzf ${path.join(__dirname, tarName).replace(/ /g, "\\ ")} -C ./build`, processOptions);
+execSync(`tar xzf ${wrapPath(path.join(__dirname, tarName))} -C ./build`, processOptions);
 execSync(`rm ${tarName}`, processOptions);
